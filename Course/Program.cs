10753ba470d9caefc17a99263564796523836a5d
Javid-C <javidia@code.edu.az>
2024-07-29 14:03:29 +0400

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nijat";
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+
+            //foreach (Student item in students)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //while (name.Length < 15)
+            /*do*///{
+                  //    name += "a";
+                  //    Console.WriteLine(name);
+                  //}
+
+            //do
+            //{
+            //    name+="a";
+            //    Console.WriteLine(name);
+            //} 
+            //while (name.Length<-1);//muddetce
+
+            int num = 2;
+
+
+            //if (num == 5)
+            //{
+            //    num += 1;
+            //    //process
+            //}
+
+            //if (num==6)
+            //{
+            //    //process
+            //}
+
+            //if (num == 5)
+            //{
+            //    num += 1;
+            //    //process
+            //}
+            //else if (num == 6)
+            //{
+            //    //process
+            //}
+            //Console.WriteLine(num);
+            ////---
+
+            //if (num == 5)
+            //{
+            //    //process
+            //}
+            //else
+            //{
+            //    //process
+            //}
+
+            //Console.WriteLine(num);
+            ////---
+
+            //if (num == 5)
+            //{
+            //    //process
+            //}else if(num == 6)
+            //{
+            //    //process
+            //}
+            //else
+            //{
+            //    //process
+            //}
+            //Console.WriteLine(num);
+            
+            switch (num)
+            {
+                case 1:// if (benzetme)
+                    //process
+                    break;
+                case 2://else if
+
+                    //process
+                    break;
+                case 3://3 ve 4 eyni else if-dir
+                case 4:
+                    //process
+                    break;
+                default://else
+                    //process
+                    break;
+            }
+        }
+    }
+}
